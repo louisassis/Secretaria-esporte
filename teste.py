65e969240a445c7d7ce6a54af3a0b8ef758582b1
luis assis <assisluis111@gmail.com>
2025-06-15 18:02:41 -0300

    final

--- teste.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-try:
-    client = MongoClient("mongodb+srv://assisluis111:senha@cluster0.0ohfzwd.mongodb.net/?retryWrites=true&w=majority")
-    client.server_info()
-    print("✅ Conexão bem-sucedida com o MongoDB Atlas!")
-except Exception as e:
-    print("❌ Erro na conexão:", e)
-
